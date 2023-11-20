2d325e2f3fd161ded44a34c53afec25f2ec4ad79
thales-pinner <thales.neves@pinpeople.com.br>
2023-11-20 13:46:14 -0300

    add devise pre config to allow params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_paramter_sanitizer.permit(:sign_up, keys: %i[name avatar])
+
+    devise_paramter_sanitizer.permit(:account_update, keys: %i[name avatar])
+  end
 end
