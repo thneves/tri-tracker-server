33e85cff0e322c7d6439405b5d4293bf5eadccc2
thales-pinner <thales.neves@pinpeople.com.br>
2023-11-20 14:41:18 -0300

    controller registritation logic

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,4 +2,22 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
+
+  private
+
+  def respond_with(current_user, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: { code: 200, message, 'Signed up Succesfully!'},
+        data: {
+          UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+        }
+      }
+    else
+      render json: {
+        status: { message: "Couldn't create user! #{current_user.errors.full_messages.to_sentence}"},
+      },
+      status: :unprocessable_entity
+    end
+  end
 end
